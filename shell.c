caf682e9382e45c0559db2b6ba5d8f259024f761
batsy255 <121512544+batsy255@users.noreply.github.com>
2023-02-08 21:14:37 -0600

    Update shell.c

--- shell.c
+++ shell.c
@@ -5,45 +5,60 @@
 #include <sys/wait.h>
 
 #define MAX_LINE 80 /* The maximum length command */
+#define MAX_COMMANDS 10 /* The maximum number of commands */
 
-void run_command(char *command) {
-    char *args[MAX_LINE/2 + 1]; /* command line arguments */
+int main(int argc, char *argv[])
+{
+    char *commands[MAX_COMMANDS][MAX_LINE/2 + 1]; /* commands */
+    int should_run = 1; /* flag to determine when to exit program */
 
-    int i = 0;
-    args[i] = strtok(command, " ");
-    while (args[i] != NULL) {
-        i++;
-        args[i] = strtok(NULL, " ");
-    }
-
-    if (strcmp(args[0], "quit") == 0) {
-        exit(0);
-    }
-
-    pid_t pid = fork();
-    if (pid < 0) {
-        fprintf(stderr, "Fork failed\n");
-        exit(1);
-    } else if (pid == 0) {
-        if (execvp(args[0], args) < 0) {
-            fprintf(stderr, "Error: command not found\n");
-            exit(1);
-        }
-    } else {
-        wait(NULL);
-    }
-}
-
-int main(int argc, char *argv[]) {
-    char input[MAX_LINE];
-    while (fgets(input, MAX_LINE, stdin) != NULL) {
-        char *token = strtok(input, ";");
-        while (token != NULL) {
-            run_command(token);
-            token = strtok(NULL, ";");
-        }
+    while (should_run) {
         printf("SHELL$ ");
         fflush(stdout);
+
+        char input[MAX_LINE];
+        fgets(input, MAX_LINE, stdin);
+        input[strcspn(input, "\n")] = 0; // remove newline character
+
+        int i = 0;
+        int background = 0;
+        char *command = strtok(input, ";");
+        while (command != NULL && i < MAX_COMMANDS) {
+            int j = 0;
+            commands[i][j] = strtok(command, " ");
+            while (commands[i][j] != NULL) {
+                j++;
+                commands[i][j] = strtok(NULL, " ");
+            }
+            if (strcmp(commands[i][j-1], "&") == 0) {
+                background = 1;
+                commands[i][j-1] = NULL;
+            }
+            i++;
+            command = strtok(NULL, ";");
+        }
+
+        for (int k = 0; k < i; k++) {
+            if (strcmp(commands[k][0], "quit") == 0) {
+                should_run = 0;
+                break;
+            }
+
+            pid_t pid = fork();
+            if (pid < 0) {
+                fprintf(stderr, "Fork failed\n");
+                exit(1);
+            } else if (pid == 0) {
+                if (execvp(commands[k][0], commands[k]) < 0) {
+                    fprintf(stderr, "Error: command not found\n");
+                    exit(1);
+                }
+            } else {
+                if (!background) {
+                    wait(NULL);
+                }
+            }
+        }
     }
 
     return 0;
