b81b9be3f1ebcf099be64aa121201b3609d501ce
batsy255 <121512544+batsy255@users.noreply.github.com>
2023-02-08 19:47:31 -0600

    Update shell.c

--- shell.c
+++ shell.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <sys/types.h>
 
 #define MAX_LINE 80 /* The maximum length command */
 
@@ -19,38 +20,43 @@ int main(int argc, char *argv[])
         fgets(input, MAX_LINE, stdin);
         input[strcspn(input, "\n")] = 0; // remove newline character
 
-        int background = 0;
-        if (input[strlen(input) - 1] == '&') {
-            background = 1;
-            input[strlen(input) - 1] = 0; // remove '&'
-        }
-
         int i = 0;
-        args[i] = strtok(input, " ");
-        while (args[i] != NULL) {
-            i++;
-            args[i] = strtok(NULL, " ");
-        }
+        char *token = strtok(input, ";");
+        while (token != NULL) {
+            args[i] = strtok(token, " ");
+            i = 0;
+            while (args[i] != NULL) {
+                i++;
+                args[i] = strtok(NULL, " ");
+            }
 
-        if (strcmp(args[0], "exit") == 0) {
-            should_run = 0;
-        } else {
-            pid_t pid = fork();
-            if (pid < 0) {
-                printf("Fork failed\n");
-                exit(1);
-            } else if (pid == 0) {
-                if (execvp(args[0], args) < 0) {
-                    printf("Error: command not found\n");
-                    exit(1);
-                }
+            int background = 0;
+            if (args[i - 1][strlen(args[i - 1]) - 1] == '&') {
+                background = 1;
+                args[i - 1][strlen(args[i - 1]) - 1] = 0; // remove '&'
+            }
+
+            if (strcmp(args[0], "exit") == 0) {
+                should_run = 0;
+                break;
             } else {
-                if (!background) {
-                    wait(NULL);
+                pid_t pid = fork();
+                if (pid < 0) {
+                    printf("Fork failed\n");
+                    exit(1);
+                } else if (pid == 0) {
+                    if (execvp(args[0], args) < 0) {
+                        printf("Error: command not found\n");
+                        exit(1);
+                    }
+                } else {
+                    if (!background) {
+                        wait(NULL);
+                    }
                 }
             }
+            token = strtok(NULL, ";");
         }
     }
-
     return 0;
 }
